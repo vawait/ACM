c4da6883f5c11633acb1259b0a207530fe2077c2
vawait <vawait@hotmail.com>
2015-03-25 21:23:06 +0800

    Codeforces Round #290 (Div. 1) D

---
+++ CodeForces/512D.cpp
@@ -0,0 +1,139 @@
+/*
+ * Author:  vawait
+ * Created Time:  2015/3/25 13:16:49
+ * Problem: CodeForces_512D
+ */
+#include<cstdio>
+#include<iostream>
+#include<cstring>
+#include<cstdlib>
+#include<cmath>
+#include<algorithm>
+#include<string>
+#include<map>
+#include<set>
+#include<vector>
+#include<queue>
+#include<stack>
+#include<ctime>
+using namespace std;
+#define rep(i, a, b) for (int i = (a); i <= (b); ++i)
+#define red(i, a, b) for (int i = (a); i >= (b); --i)
+#define clr( x , y ) memset(x,y,sizeof(x))
+#define mp make_pair
+#define pb push_back
+#define x first
+#define y second
+#define sqr(x) ((x) * (x))
+typedef long long lint;
+const lint mm = 1e9 + 9;
+const int maxn = 110;
+int n , m , vis[maxn] , cir[maxn] , sum[maxn];
+lint F[maxn] , f[maxn][maxn] , C[maxn][maxn] , ans[maxn] , fa[maxn];
+vector < int > g[maxn] , group[maxn];
+
+void DfsFind(int t,int fa,int now)
+{
+    vis[t] = 1;
+    red(i,g[t].size()-1,0) if ( g[t][i] != fa ) {
+        if ( g[t][i] == now ) cir[now] = 1;
+        if ( !vis[g[t][i]] ) DfsFind( g[t][i] , t , now );
+    }
+}
+
+lint find(lint t)
+{
+    return fa[t] == t ? t : ( fa[t] = find( fa[t] ) );
+}
+
+lint km(lint a,lint n)
+{
+    lint s = 1;
+    for ( ; n; n >>= 1 ) {
+        if ( n & 1 ) ( s *= a ) %= mm;
+        ( a *= a ) %= mm;
+    }
+    return s;
+}
+
+void init()
+{
+    int x , y;
+    rep(i,1,n) fa[i] = i , g[i].clear() , group[i].clear();
+    rep(i,1,m) {
+        scanf("%d%d",&x,&y);
+        g[x].pb( y );
+        g[y].pb( x );
+    }
+    
+    clr( cir , 0 );
+    rep(i,1,n) {
+        clr( vis , 0 );
+        DfsFind( i , 0 , i );
+    }
+    
+    rep(i,0,n)
+        rep(j,0,i) C[i][j] = j ? ( C[i-1][j-1] + C[i-1][j] ) % mm : 1;
+    
+    rep(i,1,n) if ( !cir[i] )
+        red(j,g[i].size()-1,0) if ( !cir[g[i][j]] ) fa[find(i)] = find( g[i][j] );
+    rep(i,1,n) if ( !cir[i] ) group[find(i)].pb( i );
+    
+    clr( vis , 0 );
+    clr( ans , 0 );
+    ans[0] = 1;
+}
+
+void merge(lint *ans,lint *f)
+{
+    red(i,n,1)
+        rep(j,1,i) ( ans[i] += f[j] * ans[i-j] % mm * C[i][j] ) %= mm;
+}
+
+void dfs(int t,int fa)
+{
+    int y;
+    clr( f[t] , 0 );
+    sum[t] = 1; f[t][0] = 1;
+    red(i,g[t].size()-1,0) {
+        y = g[t][i];
+        if ( cir[y] || y == fa ) continue;
+        dfs( y , t );
+        merge( f[t] , f[y] );
+        sum[t] += sum[y];
+    }
+    f[t][sum[t]] = f[t][sum[t]-1];
+}
+
+void work()
+{
+    int x , s;
+    rep(i,1,n) if ( !cir[i] )
+        red(j,g[i].size()-1,0) if ( cir[g[i][j]] ) {
+            vis[find(i)] = 1;
+            dfs( i , 0 );
+            merge( ans , f[i] );
+            break;
+        }
+    rep(gg,1,n) if ( !group[gg].empty() && !vis[gg] ) {
+        s = group[gg].size();
+        clr( F , 0 );
+        rep(i,0,s-1) {
+            x = group[gg][i];
+            dfs( x , 0 );
+            rep(j,0,s) ( F[j] += f[x][j] ) %= mm;
+        }
+        rep(i,0,s-1) ( F[i] *= km( s - i , mm - 2 ) ) %= mm;
+        merge( ans , F );
+    }
+    rep(i,0,n) printf("%I64d\n",ans[i]);
+}
+
+int main()
+{
+    while ( cin >> n >> m ) {
+        init();
+        work();
+    }
+    return 0;
+}
